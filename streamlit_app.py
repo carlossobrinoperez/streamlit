27247a60fa8256cb624f52435e57738bbe481f03
carlossobrinoperez <146819165+carlossobrinoperez@users.noreply.github.com>
2023-11-28 15:49:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
 import snowflake.connector
 
+import streamlit as st
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+st.markdown("# Main page")
+st.sidebar.markdown("# Main page")
