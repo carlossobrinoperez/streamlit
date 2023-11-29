f2731007b6b22a0d3e97326c0752c063c87d715e
carlossobrinoperez <146819165+carlossobrinoperez@users.noreply.github.com>
2023-11-29 14:45:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,10 @@ st.markdown("# Main page")
 st.sidebar.markdown("# Main page")
 
 def create_session():
-    connection_parameters = st.secrets["snowflake"]
+    connection_parameters = {
+        "account": "vpdyjth-ft62604",
+        "user": "Carlos",
+        "password": "Temp1234"}
     return Session.builder.configs(connection_parameters).create()
   
 session = create_session()
