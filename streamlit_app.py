fb5196a8b1000d65362a8a447f3bd5130c1fa5a0
carlossobrinoperez <146819165+carlossobrinoperez@users.noreply.github.com>
2023-11-29 14:29:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
-import streamlit
-import snowflake.connector
-
+from snowflake.snowpark import Session
 import streamlit as st
 
 st.markdown("# Main page")
 st.sidebar.markdown("# Main page")
+
+def create_session():
+    return Session.builder.configs(st.secrets.snowflake).create()
+  
+session = create_session()
+st.success("Connected to Snowflake!")
+
