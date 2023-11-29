1fdbe84ba09dbcee50408f90a743bde0f67867e3
carlossobrinoperez <146819165+carlossobrinoperez@users.noreply.github.com>
2023-11-29 17:11:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -218,4 +218,4 @@ ORDER BY
 #st.dataframe(df_q_sec)
 
 st.write("Duracion media de queries por dia (En segundos)")
-st.line_chart(df_q_sec, y= "DURACION_MEDIA_EN_SEGUNDOS" , x="DAY_FORMATO" )
+st.line_chart(df_q_sec, y= "DURACION_MEDIA_MILISEGUNDOS" , x="DAY_FORMATO" )
