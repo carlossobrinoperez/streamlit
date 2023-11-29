f12fcb122c7292dfe6229048dd1e5ec7a1cadaa5
carlossobrinoperez <146819165+carlossobrinoperez@users.noreply.github.com>
2023-11-29 14:15:45 +0100

    Update Credit_consumption.py

--- pages/Credit_consumption.py
+++ pages/Credit_consumption.py
@@ -1,7 +1,4 @@
 # Import libraries
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import sum, col
-import altair as alt
 import streamlit as st
 
 st.markdown("# Credit_consumption.")
