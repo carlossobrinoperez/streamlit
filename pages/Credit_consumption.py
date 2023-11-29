a19592cddeb1b9976ea1253b273bf4ff6244b244
carlossobrinoperez <146819165+carlossobrinoperez@users.noreply.github.com>
2023-11-29 12:46:24 +0100

    Update Credit_consumption.py

--- pages/Credit_consumption.py
+++ pages/Credit_consumption.py
@@ -1,5 +1,19 @@
 import streamlit as st
 import snowflake.connector
+from snowflake.snowpark import Session
+
 
 st.markdown("# Credit_consumption.")
 st.sidebar.markdown("# Credit_consumption.")
+
+connection_parameters = {
+"account": "vpdyjth-ft62604",
+"user": "Carlos",
+"password": "Temp1234",
+ }  
+
+session = Session.builder.configs(connection_parameters).create()
+
+
+df_sql = snowpark.session.sql("select ROUND(sum(credits_used)) from snowflake.account_usage.metering_history")
+df_sql.show()
