b6254fdbf97cf2ef65bca7730efa686f95a23996
carlossobrinoperez <146819165+carlossobrinoperez@users.noreply.github.com>
2023-11-29 15:55:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,42 @@ session = create_session()
 st.success("Connected to Snowflake!")
 
 st.title("Top 10 queris lanzadas por Users")
-df = session.sql("select user_name, count(*) as Queries from snowflake.account_usage.query_history group by 1 order by Queries desc limit 10")
+df = session.sql("WITH ConsultasEsteMes AS (
+  SELECT
+    TO_CHAR(DATE_TRUNC('MONTH', END_TIME), 'MM/YYYY') AS MES_FORMATO,
+    COUNT(*) AS NUMERO_DE_CONSULTAS_EXITOSAS
+  FROM
+    ACCOUNT_USAGE.QUERY_HISTORY
+  WHERE
+    EXECUTION_STATUS = 'SUCCESS' -- Solo consultas exitosas
+    AND DATE_TRUNC('MONTH', END_TIME) = DATE_TRUNC('MONTH', CURRENT_DATE()) -- Consultas del mes actual
+  GROUP BY
+    MES_FORMATO
+),
+ConsultasMesAnterior AS (
+  SELECT
+    TO_CHAR(DATE_TRUNC('MONTH', END_TIME), 'MM/YYYY') AS MES_FORMATO,
+    COUNT(*) AS NUMERO_DE_CONSULTAS_EXITOSAS_MES_ANTERIOR
+  FROM
+    ACCOUNT_USAGE.QUERY_HISTORY
+  WHERE
+    EXECUTION_STATUS = 'SUCCESS' -- Solo consultas exitosas
+    AND DATE_TRUNC('MONTH', END_TIME) = DATE_TRUNC('MONTH', DATEADD(MONTH, -1, CURRENT_DATE())) 
+	GROUP BY MES_FORMATO)
+SELECT
+  MES_FORMATO,
+  NUMERO_DE_CONSULTAS_EXITOSAS,
+  NULL AS NUMERO_DE_CONSULTAS_EXITOSAS_MES_ANTERIOR
+FROM
+  ConsultasEsteMes
+UNION ALL
+SELECT
+  MES_FORMATO,
+  NULL AS NUMERO_DE_CONSULTAS_EXITOSAS,
+  NUMERO_DE_CONSULTAS_EXITOSAS_MES_ANTERIOR
+FROM
+  ConsultasMesAnterior
+ORDER BY
+  MES_FORMATO DESC")
 st.dataframe(df)
-st.bar_chart(df, x="USER_NAME", y= "QUERIES")
 
