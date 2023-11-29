98df1c4ed53c2b37964a3b87a1d7f95b2a8c753d
carlossobrinoperez <146819165+carlossobrinoperez@users.noreply.github.com>
2023-11-29 14:15:07 +0100

    Update Credit_consumption.py

--- pages/Credit_consumption.py
+++ pages/Credit_consumption.py
@@ -7,3 +7,9 @@ import streamlit as st
 st.markdown("# Credit_consumption.")
 st.sidebar.markdown("# Credit_consumption.")
 
+conn = st.connection("snowflake")
+
+df = conn.query("select user_name, count(*) as Queries  from snowflake.account_usage.query_history;", ttl=600)
+
+st.write(df)
+
